5ff87d6b2b14446d4856354121d175b2e3661efc
RezkyPratiwi <2211104029@ittelkom-pwt.ac.id>
2025-04-20 23:26:04 +0700

    first commit

---
+++ 07_Grammar-Based_Input_Processing_Parsing/TP07/DataMahasiswa2211104029.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+class DataMahasiswa2211104005
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    public class Mahasiswa
+    {
+        public Nama nama { get; set; }
+        public long nim { get; set; }
+        public string fakultas { get; set; }
+    }
+
+    public static void ReadJSON()
+    {
+        string filePath = "tp7_1_2211104029.json";  // Sesuaikan jika ada folder "Data/"
+        string jsonString = File.ReadAllText(filePath);
+
+        Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+        Console.WriteLine($"Nama {mhs.nama.depan} {mhs.nama.belakang} dengan NIM {mhs.nim} dari Fakultas {mhs.fakultas}");
+    }
+}
---
+++ 07_Grammar-Based_Input_Processing_Parsing/TP07/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static void Main()
+    {
+        DataMahasiswa2211104005.ReadJSON();
+        KuliahMahasiswa2211104005.ReadJSON();
+    }
+}
